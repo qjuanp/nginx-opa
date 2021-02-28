c48f023a7ba39f52a384719ab342da64d74b0250
Juan P <code@qjuanp.dev>
2021-02-28 18:26:08 +0000

    Validation against OPA

--- nginx_conf.d/opa.js
+++ nginx_conf.d/opa.js
@@ -1,24 +1,43 @@
-export default { validate }
+export default { validate, ok }
 
 var fs = require("fs");
 
 function validate(request) {
-    request.error("Logging info ", request.variables.request_body)
-    var body = JSON.parse(request.variables.request_body)
+    request.error("Logging info " + request.requestText)
+
     var opa_query = {
-        input: body
+        input: JSON.parse(request.requestText)
     }
 
-    request.error("Logging info " + opa_query)
+    request.error("Logging info " + JSON.stringify(opa_query))
 
-    return request.subrequest("/_send_opa_query", { method: "POST", body: JSON.stringify(opa_query) })
+    request.subrequest("/_send_opa_query", { method: "POST", body: JSON.stringify(opa_query) })
         .then(reply => JSON.parse(reply.responseBody))
-        .then(response => {
-            if (response.result) {
-                request.return(204);
-            } else {
-                request.return(403);
-            }
-        })
+        .then(response => handleOpaResponse(request, response))
         .catch(e => r.return(401));
+}
+
+function handleOpaResponse(originalRequest, response) {
+    if (response.result) {
+        originalRequest.error("VALID request body")
+        originalRequest.subrequest('/_ok/', originalRequest.variables.args, done(originalRequest))
+    } else {
+        originalRequest.error("Invalid request body")
+        originalRequest.return(403)
+    }
+}
+
+function done(originalRequest) {
+    return function (response) {
+        return originalRequest.return(response.status, response.responseBody)
+    }
+}
+
+function ok(request) {
+    request.error("OK Function")
+    var response = {
+        validation: "OK",
+        data: JSON.parse(request.requestText)
+    }
+    request.return(200, JSON.stringify(response))
 }
